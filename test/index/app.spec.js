2e7a172631e7b2845c3f69ac1749e7a07ad234ba
梁伟文 <liangweiwen@meituan.com>
2017-07-31 13:28:28 +0800

    feat: add test for event

--- test/index/app.spec.js
+++ test/index/app.spec.js
@@ -18,36 +18,51 @@ describe('App', function() {
     let cb2 = sinon.spy()
     dummy = new appForTest
 
-    describe('on', function() {
+    describe('#on', function() {
 
-      it('should bind event', function() {
-        dummy.on('test', cb1)
-        expect(dummy.handlers).to.be.a('object').with.property('test').to.be.a('array').lengthOf(1)
+      context('when called at the first time', function() {
+        it('should bind event', function() {
+          dummy.on('test', cb1)
+          expect(dummy.handlers).to.be.a('object').with.property('test').to.be.a('array').lengthOf(1)
+        })
       })
 
-      it('should store event in array', function() {
-        dummy.on('test', cb2);
-        expect(dummy.handlers.test).lengthOf(2)
-      })
-    })
-
-    describe('off', function() {
-
-      it('should remove a callback', function() {
-        expect(dummy.handlers.test).lengthOf(2)
-        dummy.off('test', cb1)
-        expect(dummy.handlers.test).lengthOf(1)
-        expect(dummy.handlers.test[0]).to.equal(cb2)
+      context('when called at second time', function() {
+        it('should store event in array', function() {
+          dummy.on('test', cb2);
+          expect(dummy.handlers.test).lengthOf(2)
+        })
       })
     })
 
-    describe('fire', function() {
+    describe('#fire', function() {
       it('should trigger callback', function() {
         const message = 'this is a test message'
         dummy.fire('test', message)
+        sinon.assert.calledWith(cb1, message)
         sinon.assert.calledWith(cb2, message)
       })
     })
+
+    describe('#off', function() {
+
+      context('with param:fn', function() {
+        it('should remove a callback', function() {
+          expect(dummy.handlers.test).lengthOf(2)
+          dummy.off('test', cb1)
+          expect(dummy.handlers.test).lengthOf(1)
+          expect(dummy.handlers.test[0]).to.equal(cb2)
+        })
+      })
+
+      context('without param:fn', function() {
+        it('should remove all callbacks', function() {
+          dummy.off('test')
+          expect(dummy.handlers).not.to.have.property('test')
+        })
+      })
+    })
+  })
   })
   
 })
