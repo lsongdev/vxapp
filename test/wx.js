a8bba10ca2b5bffd4fe96114ac09198393f1a636
梁伟文 <liangweiwen@meituan.com>
2017-07-31 13:26:39 +0800

    feat(wx.js): add new shim

--- test/wx.js
+++ test/wx.js
@@ -2,5 +2,10 @@ export const wx = {
   onCompassChange: function() {},
   onAccelerometerChange: function() {},
   startCompass: function() {},
-  startAccelerometer: function() {}
+  startAccelerometer: function() {},
+
+  removeStorageSync: function() {},
+  clearStorage: function() {},
+  setStorageSync: function() {},
+  getStorageSync: function() {}
 }
