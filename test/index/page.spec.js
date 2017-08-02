f492e7ba94d608e750a1a412943d1e681fe59822
梁伟文 <liangweiwen@meituan.com>
2017-08-02 13:08:35 +0800

    update: add hooks

--- test/index/page.spec.js
+++ test/index/page.spec.js
@@ -3,12 +3,25 @@ import { expect, assert, should } from 'chai'
 import sinon from 'sinon'
 
 import { wx } from '../wx'
-import { vxapp } from '../vxapp'
+import * as mp from '../mini-program'
+import { vxapp, vxapp$run } from '../vxapp'
 
 global.wx = wx
 
 describe('Page', function() {
   class pageForTest extends vxapp.Page {}
+  let page
+  let sandbox
+
+  beforeEach(function() {
+    page = vxapp$run(pageForTest, mp.Page)
+    sandbox = sinon.sandbox.create()
+  })
+
+  afterEach(function() {
+    page = null
+    sandbox.restore()
+  })
   
   describe('#setData', function() {
     it('should set data')
