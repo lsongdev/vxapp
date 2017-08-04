ecc20e0f9cccfa4286b68b9ea2e6d0ffebd30c81
梁伟文 <liangweiwen@meituan.com>
2017-08-04 15:49:53 +0800

    test: add test for page
    #setData #onLoad #onPullDownRefresh #onReachBottom

--- test/index/page.spec.js
+++ test/index/page.spec.js
@@ -14,28 +14,65 @@ describe('Page', function() {
   let sandbox
 
   beforeEach(function() {
-    page = vxapp$run(pageForTest, mp.Page)
+    page = new pageForTest
     sandbox = sinon.sandbox.create()
+    page.$ctx = {
+      setData: sandbox.stub()
+    }
   })
 
   afterEach(function() {
-    page = null
     sandbox.restore()
+    page = null
   })
   
   describe('#setData', function() {
-    it('should set data')
+    it('should set data', function() {
+      const args = {
+        str: 'test'
+      }
+
+      page.setData(args)
+
+      sinon.assert.calledWith(page.$ctx.setData, args)
+    })
   })
 
   describe('#onLoad', function() {
-    it('should set data on loading')
+    it('should set data on loading', async function() {
+      const args = {
+        str: 'test'
+      }
+
+      await page.onLoad(args)
+
+      sinon.assert.calledWith(page.$ctx.setData, {options: args})
+    })
   })
 
   describe('#onPullDownRefresh', function() {
-    it('should fetch data')
+    it('should fetch data', async function() {
+      const list = [1, 2, 3]
+      page.onFetch = page.onFetch || (function() {})
+      sandbox.stub(page, 'onFetch').resolves(list)
+
+      await page.onPullDownRefresh()
+
+      sinon.assert.called(page.onFetch)
+      sinon.assert.called(page.$ctx.setData)
+    })
   })
 
   describe('#onReachBottom', function() {
-    it('should fetch data')
+    it('should fetch data', async function() {
+      const list = [1, 2, 3]
+      page.onFetch = page.onFetch || (function() {})
+      sandbox.stub(page, 'onFetch').resolves(list)
+
+      await page.onReachBottom()
+
+      sinon.assert.called(page.onFetch)
+      sinon.assert.called(page.$ctx.setData)
+    })
   })
 })
